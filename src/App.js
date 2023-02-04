5d933fcf2dd679e5d0bb1a3403fe059fa03ad185
Nicolas Ettlin <nicolas.ettlin@me.com>
2023-02-03 21:06:57 -0500

    Fix the tab bar animation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import React, { lazy, Suspense, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import ListPage from "./pages/ListPage";
+import MapPage from "./pages/MapPage";
+import NotFoundPage from "./pages/NotFoundPage";
 import queryLocations from "./util/queryLocations";
 import "./App.css";
-import NotFoundPage from "./pages/NotFoundPage";
-
-const MapPage = lazy(() => import('./pages/MapPage'));
 
 function App() {
   // Load locations
@@ -22,28 +21,26 @@ function App() {
 
   return (
     <React.StrictMode>
-      <Suspense fallback={<div />}>
-        <BrowserRouter>
-          <div className="App">
-            <Routes>
-              <Route
-                path="/"
-                element={<ListPage locations={locations} />}
-              />
-              <Route
-                path="/map"
-                element={<MapPage locations={locations} />}
-              />
-              <Route
-                path="*"
-                element={<NotFoundPage />}
-              />
-            </Routes>
-          </div>
+      <BrowserRouter>
+        <div className="App">
+          <Routes>
+            <Route
+              path="/"
+              element={<ListPage locations={locations} />}
+            />
+            <Route
+              path="/map"
+              element={<MapPage locations={locations} />}
+            />
+            <Route
+              path="*"
+              element={<NotFoundPage />}
+            />
+          </Routes>
+        </div>
 
-          <Navbar />
-        </BrowserRouter>
-      </Suspense>
+        <Navbar />
+      </BrowserRouter>
     </React.StrictMode>
   );
 }
