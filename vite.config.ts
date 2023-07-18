72b10dd9e6ab1772dad0bb81f5386d9049cbef03
Jaisal Patel <70134418+GhostOf0days@users.noreply.github.com>
2023-07-18 19:56:00 -0400

    Fixed MapkitJS environment variable config (#83)

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,28 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import { checker } from 'vite-plugin-checker';
 import react from '@vitejs/plugin-react-swc';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 import svgrPlugin from 'vite-plugin-svgr';
 import eslint from 'vite-plugin-eslint';
 
-const viteEnv = {}
-Object.keys(process.env).forEach((key) => {
-  if (key.startsWith(`VITE_`)) {
-    viteEnv[`import.meta.env.${key}`] = process.env[key]
-  }
-})
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  define: viteEnv,
-  build: {
-    outDir: 'build',
-  },
-  plugins: [
-    react(), 
-    viteTsconfigPaths(),
-    svgrPlugin(),
-    eslint(),
-    checker({
-      typescript: true
-    })
-  ],
+  return {
+    define: {
+      'process.env.VITE_MAPKITJS_TOKEN': JSON.stringify(env.VITE_MAPKITJS_TOKEN),
+    },
+    build: {
+      outDir: 'build',
+    },
+    plugins: [
+      react(),
+      viteTsconfigPaths(),
+      svgrPlugin(),
+      eslint(),
+      checker({
+        typescript: true,
+      }),
+    ],
+  };
 });
