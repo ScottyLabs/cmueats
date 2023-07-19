822baa8ab8160f23c092cfc94f6338113f8ba2a1
Jaisal Patel <70134418+GhostOf0days@users.noreply.github.com>
2023-07-19 19:15:28 -0400

    Removed unused code

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import { registerSW } from 'virtual:pwa-register';
 
 import App from './App';
-
-// Refreshes (comment out code in development, so doesn't refresh to black screen)
-const updateSW = registerSW({
-	onNeedRefresh() {
-		updateSW(true);
-	},
-});
 
 const rootElement = document.getElementById('root');
 
