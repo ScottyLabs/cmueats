69cf35a043c16e70cfdca21b742aacb470b92a8b
Jaisal Patel <70134418+GhostOf0days@users.noreply.github.com>
2023-12-02 17:18:08 -0500

    Fix linter issue

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import App from './App';
 import { createRoot } from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './index.css';
+import App from './App';
 
 const queryClient = new QueryClient();
 
