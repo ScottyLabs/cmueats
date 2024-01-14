0fbdcadf63c9bbcbec21b1b915f5a4318ff6d924
cirex <cirex314@gmail.com>
2024-01-14 17:05:29 -0400

    minor code cleanup with fuzzy search + mutating state variable bug fix (#422)
    * minor code cleanup
    
    * don't mutate filteredLocations
    
    * moved comment
    
    * Fixed linter failure
    
    ---------
    
    Co-authored-by: Jaisal Patel <70134418+GhostOf0days@users.noreply.github.com>
    Co-authored-by: GhostOf0days <jaisal.patel45@gmail.com>

--- src/pages/ListPage.tsx
+++ src/pages/ListPage.tsx
@@ -86,13 +86,16 @@ function ListPage({
 
 	useLayoutEffect(() => {
 		if (locations === undefined || fuse === null) return;
+		const processedSearchQuery = searchQuery.trim().toLowerCase();
 
 		// Fuzzy search. If there's no search query, it returns all locations.
-		const searchResults = searchQuery.trim()
-			? fuse.search(searchQuery.trim().toLowerCase())
-			: locations.map((location) => ({ item: location }));
-		const filteredResults = searchResults.map((result) => result.item);
-		setFilteredLocations(filteredResults);
+		setFilteredLocations(
+			processedSearchQuery.length === 0
+				? locations
+				: fuse
+						.search(processedSearchQuery)
+						.map((result) => result.item),
+		);
 	}, [searchQuery, fuse, locations]);
 
 	// const [showAlert, setShowAlert] = useState(true);
@@ -172,7 +175,7 @@ function ListPage({
 						);
 					return (
 						<Grid container spacing={2}>
-							{filteredLocations
+							{[...filteredLocations]
 								.sort((location1, location2) => {
 									const state1 = location1.locationState;
 									const state2 = location2.locationState;
