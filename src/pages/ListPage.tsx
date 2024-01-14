425600efbdf84d073fbc91e01beb426045079c12
topshenyi-web <erxu@ctemc.org>
2024-01-14 11:35:08 -0400

    revert lint.yml and add vitest.yml

--- src/pages/ListPage.tsx
+++ src/pages/ListPage.tsx
@@ -163,22 +163,24 @@ function ListPage({
 					return (
 						<Grid container spacing={2}>
 							{filteredLocations
-								.sort((l1, l2) => {
-									const state1 = l1.locationState;
-									const state2 = l2.locationState;
+								.sort((location1, location2) => {
+									const state1 = location1.locationState;
+									const state2 = location2.locationState;
 									if (state1 !== state2)
 										return state1 - state2;
 									// this if statement is janky but otherwise TS won't
 									// realize that the timeUntil property exists on both l1 and l2
 									if (
-										l1.closedLongTerm ||
-										l2.closedLongTerm
+										location1.closedLongTerm ||
+										location2.closedLongTerm
 									) {
 										assert(
-											l1.closedLongTerm &&
-												l2.closedLongTerm,
+											location1.closedLongTerm &&
+												location2.closedLongTerm,
 										);
-										return l1.name.localeCompare(l2.name);
+										return location1.name.localeCompare(
+											location2.name,
+										);
 									}
 									// flip sorting order if locations are both open or opening soon
 									return (
@@ -186,7 +188,8 @@ function ListPage({
 										state1 === LocationState.OPENS_SOON
 											? -1
 											: 1) *
-										(l1.timeUntil - l2.timeUntil)
+										(location1.timeUntil -
+											location2.timeUntil)
 									);
 								})
 								.map((location) => (
