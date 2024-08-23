1b619eec1ef24ca1c1decb39af616e1f75ed0d87
Jaisal Patel <70134418+GhostOf0days@users.noreply.github.com>
2024-08-23 11:53:20 -0400

    emergency fix (#509)

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ import {
 	IReadOnlyLocation,
 } from './types/locationTypes';
 
-const CMU_EATS_API_URL = 'https://dining.apis.scottylabs.org/locations';
+const CMU_EATS_API_URL = '/emergency_fix.json';
 // const CMU_EATS_API_URL = 'http://localhost:5173/example-response.json'; // for debugging purposes (note that you need an example-response.json file in the /public folder)
 // const CMU_EATS_API_URL = 'http://localhost:5010/locations'; // for debugging purposes (note that you need an example-response.json file in the /public folder)
 function App() {
