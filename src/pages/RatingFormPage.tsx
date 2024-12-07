082ac7cd45d796898ead0bad04eb91fd0369024d
Talal <thali@andrew.cmu.edu>
2024-12-07 20:30:57 +0300

    Added food experience describtion input and the restaurant selection dropdown with all of the restaurants of CMUeats website

--- src/pages/RatingFormPage.tsx
+++ src/pages/RatingFormPage.tsx
@@ -37,6 +37,65 @@ const RatingFormPage = () => {
           onChange={(e) => setUserEmail(e.target.value)}
         />
 
+        {/* Restaurant Selection */}
+        <TextField
+          select
+          label="Select Restaurant"
+          SelectProps={{ native: true }}
+          variant="outlined"
+          fullWidth
+          required
+          value={selectedRestaurant}
+          onChange={(e) => setSelectedRestaurant(e.target.value)}
+        >
+    <option value="">-- Select a Restaurant --</option>
+  <option value="stephanies@example.com">Stephanie's - Market c</option>
+  <option value="scottys@example.com">Scotty's Market By Salem's</option>
+  <option value="aubonpain@example.com">Au Bon Pain At Skibo Café</option>
+  <option value="rohrcafe@example.com">Rohr Café - La Prima</option>
+  <option value="schatz@example.com">Schatz Dining Room</option>
+  <option value="defercafe@example.com">De Fer Coffee & Tea At Maggie Murph Café</option>
+  <option value="exchange@example.com">The Exchange</option>
+  <option value="capitalgrains@example.com">Capital Grains - Rohr Commons</option>
+  <option value="milliescreamery@example.com">Millie's Coffee 'n' Creamery - Rohr Commons</option>
+  <option value="stackddessert@example.com">Stack'd Dessert Bar</option>
+  <option value="stackdunderground@example.com">Stack'd Underground</option>
+  <option value="entropy@example.com">Entropy+</option>
+  <option value="elgallo@example.com">El Gallo De Oro</option>
+  <option value="revolutionnoodle@example.com">Revolution Noodle</option>
+  <option value="tasteofindia@example.com">Taste Of India</option>
+  <option value="trueburger@example.com">True Burger</option>
+  <option value="hunanexpress@example.com">Hunan Express</option>
+  <option value="edgecafe@example.com">The Edge Cafe & Market</option>
+  <option value="tahini@example.com">Tahini</option>
+  <option value="eggshoppe@example.com">Egg Shoppe - Grubhub Only</option>
+  <option value="laprimaespresso@example.com">La Prima Espresso</option>
+  <option value="redhawkcoffee@example.com">Redhawk Coffee</option>
+  <option value="zebralounge@example.com">Zebra Lounge</option>
+  <option value="ciaobella@example.com">Ciao Bella</option>
+  <option value="crispandcrust@example.com">Crisp And Crust</option>
+  <option value="nourish@example.com">Nourish</option>
+  <option value="buildpizza@example.com">Build Pizza - Rohr Commons</option>
+  <option value="forbesavesubs@example.com">Forbes Avenue Subs - Rohr Commons</option>
+  <option value="rohrcommons@example.com">Rohr Commons - Tepper Eatery</option>
+  <option value="teppertaqueria@example.com">Tepper Taqueria</option>
+  <option value="teppertaqueriaexp@example.com">Tepper Taqueria Express</option>
+  <option value="wildbluesushi@example.com">Wild Blue Sushi - Ruge Atrium</option>
+  <option value="shakesmart@example.com">Shake Smart</option>
+  <option value="eatevenings@example.com">E.a.t. (evenings At Tepper) - Rohr Commons</option>
+  <option value="urbanrevolution@example.com">Urban Revolution - Grubhub Only</option>
+  <option value="testconcept@example.com">Test Concept</option>
+  <option value="tartantruck@example.com">Tartan Food Truck - Mr. Bulgogi</option>
+</TextField>
 
-
-
+        {/* Food Experience */}
+        <TextField
+          variant="outlined"
+          label="Describe Your Food Experience"
+          multiline
+          rows={3}
+          fullWidth
+          required
+          value={foodExperience}
+          onChange={(e) => setFoodExperience(e.target.value)}
+        />
