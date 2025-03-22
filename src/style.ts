c960b6427e92efc6d4e3f838da40b3e0f89e4ba1
laasyaaki <laasyaaki>
2025-03-22 16:17:09 -0400

    chnaged banner font for small screens

---
+++ src/style.ts
@@ -0,0 +1,31 @@
+import { Button, styled, Typography } from '@mui/material';
+
+const ErrorTitle = styled(Typography)({
+	color: 'white',
+	marginBottom: 12,
+	fontSize: 24,
+	fontFamily:
+		'"Zilla Slab", "Inter", -apple-system, BlinkMacSystemFont, "Segoe UI", ' +
+		'"Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", ' +
+		'"Droid Sans", "Helvetica Neue", sans-serif',
+	fontWeight: 600,
+});
+
+const ErrorText = styled(Typography)({
+	color: '#d4d4d8',
+	marginBottom: 20,
+	fontSize: 16,
+});
+
+const ErrorButton = styled(Button)({
+	fontWeight: 600,
+	fontFamily:
+		'"Inter", -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", ' +
+		'"Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", ' +
+		'"Helvetica Neue", sans-serif',
+	color: 'white',
+	backgroundColor: '#1D1F21',
+	elevation: 30,
+});
+
+export { ErrorTitle, ErrorText, ErrorButton };
