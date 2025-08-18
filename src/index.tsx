65deb2c997b42705c8d0fa58b6f5aa1456fb6117
topshenyi-web <erxu@ctemc.org>
2025-08-18 19:25:58 -0400

    fix: report all unhandled frontend errors

--- src/index.tsx
+++ src/index.tsx
@@ -7,11 +7,19 @@ import { PostHogProvider } from 'posthog-js/react';
 import App from './App';
 import { ThemeProvider } from './ThemeProvider';
 import env from './env';
+import notifySlack from './util/slack';
 
 posthog.init(env.VITE_POSTHOG_KEY || '', {
     person_profiles: 'identified_only',
 });
+// error handling
+// catches synchronous errors
+window.addEventListener('error', (event) => {
+    notifySlack(`<!channel> ${event.error.message}\n${event.error.stack}`);
+});
 
+// catches errors raised in try-catch blocks
+window.addEventListener('unhandledrejection', (er) => notifySlack(`<!channel> ${er.reason}\n${er.reason.stack}`));
 const rootElement = document.getElementById('root');
 
 if (rootElement) {
