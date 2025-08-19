e503cc1571a7b2172fdd2463a2956df65f06d34d
Eric Xu <cirex314@gmail.com>
2025-08-19 08:59:32 -0400

    report all unhandled errors to slack (#660)
    * fix: report all unhandled frontend errors
    
    * chore: reword error
    
    * fix: catch failed notifyslacks to prevent infinite loop
    
    * fix: move bg color from .App to body
    
    * fix: add user agent string to slack debug

--- src/util/slack.ts
+++ src/util/slack.ts
@@ -4,7 +4,7 @@ import env from '../env';
 export default async function notifySlack(message: string) {
     await axios.post(
         `${env.VITE_API_URL}/api/sendSlackMessage`, // we can't directly call the Slack webhook because they don't support CORS preflight checking https://github.com/slackapi/node-slack-sdk/issues/1568
-        { message },
+        { message: `${message}\nBrowser info: \`${navigator.userAgent}\`` },
         {
             headers: {
                 'Content-Type': 'application/json',
