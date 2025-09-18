017011dba61534da167f8c6c23e6023787053e03
laasyaaki <laasyaaki>
2025-09-18 18:50:09 -0400

    lint

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import * as motion from 'motion/react-client';
 import clsx from 'clsx';
 import css from './Navbar.module.css';
