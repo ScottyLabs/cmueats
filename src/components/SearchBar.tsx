83e1e195b7692fe65242d72dac159113db0d76a0
Aromia <f2940015504@gmail.com>
2025-09-18 22:48:41 -0400

    fix: not function eslint error & chore: lowercase

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,12 +1,7 @@
 import React, { useEffect, useRef } from 'react';
 import './SearchBar.css';
 
-type SearchBarProps = {
-    searchQuery: string;
-    setSearchQuery: React.Dispatch<string>;
-};
-
-const SearchBar = ({ searchQuery, setSearchQuery }: SearchBarProps) => {
+function SearchBar({ searchQuery, setSearchQuery }: { searchQuery: string; setSearchQuery: React.Dispatch<string> }) {
     const inputRef = useRef<HTMLInputElement>(null);
 
     useEffect(() => {
@@ -29,20 +24,20 @@ const SearchBar = ({ searchQuery, setSearchQuery }: SearchBarProps) => {
     }, []);
 
     return (
-        <div className="Locations-search-wrapper">
+        <div className="locations-search-wrapper">
             <input
                 ref={inputRef}
-                className="Locations-search"
+                className="locations-search"
                 type="search"
                 value={searchQuery}
                 onChange={(e) => setSearchQuery(e.target.value)}
                 placeholder=""
             />
-            <div className="Locations-search-hint">
+            <div className="locations-search-hint">
                 Type <kbd>/</kbd> to search
             </div>
         </div>
     );
-};
+}
 
 export default SearchBar;
