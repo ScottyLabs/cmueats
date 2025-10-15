e2557df3471a148610bf9b6aa308768ac0914356
Aromia <f2940015504@gmail.com>
2025-10-15 14:55:54 -0400

    chore: drawer minimal style

--- src/components/DrawerHeader.tsx
+++ src/components/DrawerHeader.tsx
@@ -1,4 +1,6 @@
 import { useContext } from 'react';
+import { X } from 'lucide-react';
+import { highlightColors } from '../constants/colors';
 import { DrawerContext } from '../contexts/DrawerContext';
 import css from './DrawerHeader.module.css';
 
@@ -8,14 +10,20 @@ function DrawerHeader() {
     const { name, statusMsg: status, location: physicalLocation } = location ?? {};
 
     return (
-        <div className={css['drawer-header']}>
-            <div>
-                <span>{name}</span>
+        <div className={css['drawer-header-container']}>
+            <div className={css['header']}>
+                <h3 className={css['drawer-loction-text']}>{name}</h3>
                 <button type="button" onClick={() => drawerContext.setIsDrawerActive(false)}>
                     close
+                    <X size={15} />
                 </button>
             </div>
-            <div>{status}</div>
+            <div
+                className={css['status-text']}
+                style={{ '--status-color': highlightColors[location?.locationState ?? 0] }}
+            >
+                {status}
+            </div>
             <div>{physicalLocation}</div>
         </div>
     );
--- src/components/DrawerTabNav.tsx
+++ src/components/DrawerTabNav.tsx
@@ -1,12 +1,14 @@
 import { useContext } from 'react';
-import css from './DrawerTabNav.module.css';
+import { TextAlignStart } from 'lucide-react';
 import { DrawerContext } from '../contexts/DrawerContext';
+import css from './DrawerTabNav.module.css';
 
 function DrawerTabNav() {
     const drawerContext = useContext(DrawerContext);
     return (
         <div className={css['drawer-tab-nav']}>
             <button type="button" onClick={() => drawerContext.setActiveTab('overview')}>
+                <TextAlignStart />
                 overview
             </button>
             {/* <button type="button" onClick={() => drawerContext.setActiveTab('menu')}>
