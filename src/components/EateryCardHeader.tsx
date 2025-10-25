e161ff774d48aec8d1ea08427e1a8557f20013aa
Aromia <f2940015504@gmail.com>
2025-10-25 17:28:14 -0400

    chore: change bar indicator to dot

--- src/components/EateryCardHeader.tsx
+++ src/components/EateryCardHeader.tsx
@@ -1,8 +1,10 @@
+import { useRef } from 'react';
 import { IReadOnlyLocation_Combined } from '../types/locationTypes';
 import { highlightColors } from '../constants/colors';
 import css from './EateryCardHeader.module.css';
 
 function EateryCardHeader({ location }: { location: IReadOnlyLocation_Combined }) {
+    const dotRef = useRef<HTMLDivElement | null>(null);
     const relativeTime = location.statusMsg.substring(0, location.statusMsg.indexOf('(') - 1);
     const absoluteTime = location.statusMsg.substring(location.statusMsg.indexOf('(')).slice(1, -1);
 
@@ -11,6 +13,12 @@ function EateryCardHeader({ location }: { location: IReadOnlyLocation_Combined }
             className={css['card-header-container']}
             style={{ '--status-color': highlightColors[location.locationState] }}
         >
+            <div
+                className={css['card-header-dot']}
+                style={{ '--status-color': highlightColors[location.locationState] }}
+                ref={dotRef}
+            />
+
             <div className={css['card-header-relative-time-text']}>{relativeTime}</div>
             <div className={css['card-header-absolute-time-text']}>{absoluteTime}</div>
         </div>
