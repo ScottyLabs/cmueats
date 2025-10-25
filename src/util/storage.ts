d4a255639f94f5ba6deda898be663e02fd14fb42
stanleymw <107821509+stanleymw@users.noreply.github.com>
2025-10-25 16:47:03 -0400

    add datafixer

--- src/util/storage.ts
+++ src/util/storage.ts
@@ -1,6 +1,18 @@
-import { CardStateMap } from '../components/EateryCard';
+import { CardStateMap, CardStatus } from '../components/EateryCard';
+
+function upgradeToCardStateMapFromOldFormat(old: string): CardStateMap {
+    const arr = JSON.parse(old);
+    return Object.fromEntries((arr as string[]).map((id) => [id, CardStatus.PINNED]));
+}
 
 export function getStateMap(): CardStateMap {
+    const old = localStorage.getItem("pinnedEateries")
+    if (old !== null) {
+        localStorage.removeItem("pinnedEateries")
+        const ret = upgradeToCardStateMapFromOldFormat(old);
+        return ret
+    }
+
     try {
         const arr: CardStateMap = JSON.parse(localStorage.getItem('eateryStates') ?? '{}');
         // return new Map(arr.map(obj => [obj.key, obj.value]));
@@ -9,6 +21,7 @@ export function getStateMap(): CardStateMap {
     } catch {
         return {};
     }
+
 }
 
 export function setLocationStateMap(obj: CardStateMap) {
