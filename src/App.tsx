1f252fb70bb52a08e4c29ba8826efca3288a114f
laasyaaki <laasya.p.aki@gmail.com>
2025-11-05 13:10:17 -0500

    bye bye nova

--- src/App.tsx
+++ src/App.tsx
@@ -88,7 +88,7 @@ function App() {
             <ErrorBoundary fallback={<ErrorBoundaryFallback />}>
                 <BrowserRouter>
                     <div className="App">
-                        <Banner />
+                        {/* <Banner /> */}
                         {/* <div className="AdBanner">
                             CMUEats is now up to date with the official dining website! Sorry for the inconvenience.
                             &gt;_&lt;
