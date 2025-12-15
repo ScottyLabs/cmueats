df490ce31ba3fa5ecf9ada270a694ac0f192065e
topshenyi-web <erxu@ctemc.org>
2025-12-14 19:51:17 -0500

    fix: lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import { DateTime } from 'luxon';
