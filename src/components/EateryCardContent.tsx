9cb8ea2d8f99cb920d526367482a9876aad48632
topshenyi-web <erxu@ctemc.org>
2025-12-14 19:31:35 -0500

    style: tweak card styling a bit

--- src/components/EateryCardContent.tsx
+++ src/components/EateryCardContent.tsx
@@ -1,24 +1,16 @@
 import { MapPin } from 'lucide-react';
-import UnpinnedControlIcon from '../assets/control_buttons/unpinned.svg?react';
-import PinnedControlIcon from '../assets/control_buttons/pinned.svg?react';
-import EyeControlIcon from '../assets/control_buttons/eye.svg?react';
-import EyeOffControlIcon from '../assets/control_buttons/eyeOff.svg?react';
 import { IReadOnlyLocation_Combined } from '../types/locationTypes';
 import { CardViewPreference } from '../util/storage';
 import css from './EateryCardContent.module.css';
 
 function EateryCardContent({
     location,
-    updateViewPreference,
     partOfMainGrid,
 }: {
     location: IReadOnlyLocation_Combined;
-    updateViewPreference: (newViewPreference: CardViewPreference) => void;
     partOfMainGrid: boolean;
 }) {
-    const { location: physicalLocation, name, cardViewPreference } = location;
-    const isPinned = cardViewPreference === 'pinned';
-    const isHidden = cardViewPreference === 'hidden';
+    const { location: physicalLocation, name } = location;
 
     return (
         <div className={css['card-content-container']}>
@@ -32,40 +24,6 @@ function EateryCardContent({
             {partOfMainGrid && (
                 <div className={css['action-bar']}>
                     <div className={css.rating}>rating placeholder</div>
-
-                    <div className={css['button-container']}>
-                        <button
-                            type="button"
-                            className={css['action-button']}
-                            aria-label={isPinned ? 'Unpin Card' : 'Pin Card'}
-                            onClick={(event) => {
-                                event.stopPropagation();
-                                updateViewPreference(isPinned ? 'normal' : 'pinned');
-                            }}
-                        >
-                            {isPinned ? (
-                                <PinnedControlIcon className={css['action-icon']} />
-                            ) : (
-                                <UnpinnedControlIcon className={css['action-icon']} />
-                            )}
-                        </button>
-
-                        <button
-                            type="button"
-                            className={css['action-button']}
-                            aria-label={isHidden ? 'Show Card' : 'Hide Card'}
-                            onClick={(event) => {
-                                event.stopPropagation();
-                                updateViewPreference(isHidden ? 'normal' : 'hidden');
-                            }}
-                        >
-                            {isHidden ? (
-                                <EyeOffControlIcon className={css['action-icon']} />
-                            ) : (
-                                <EyeControlIcon className={css['action-icon']} />
-                            )}
-                        </button>
-                    </div>
                 </div>
             )}
         </div>
