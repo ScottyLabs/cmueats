dde11ee70aeac0a955b1275251ef1d4d6637f2f0
topshenyi-web <erxu@ctemc.org>
2025-12-31 16:08:12 -0500

    style: add styles to log in button

--- src/components/AuthBanner.tsx
+++ src/components/AuthBanner.tsx
@@ -2,12 +2,13 @@ import { useLayoutEffect, useState } from 'react';
 import css from './AuthBanner.module.css';
 import XIcon from '../assets/control_buttons/x.svg?react';
 
+/** Used for notifying the user if there was a problem signing in */
 export default function AuthBanner() {
     const [shouldDisplay, setShouldDisplay] = useState(false);
     useLayoutEffect(() => {
         if (new URL(window.location.href).searchParams.get('AUTH_FAILED') !== null) {
             setShouldDisplay(true);
-            window.history.replaceState({}, document.title, window.location.pathname);
+            window.history.replaceState({}, document.title, window.location.pathname); // strip query param
         }
     }, []);
     return (
--- src/components/ListPageHeader.tsx
+++ src/components/ListPageHeader.tsx
@@ -2,20 +2,44 @@ import { useMemo } from 'react';
 import { $api, login, logout } from '../api';
 import css from './ListPageHeader.module.css';
 import { getGreetings } from '../util/greeting';
+import GoogleIcon from '../assets/google.svg?react';
+import SignOut from '../assets/control_buttons/signOut.svg?react';
 
 export default function ListPageHeader() {
-    const { data: userLoggedInData, isLoading } = $api.useQuery('get', '/whoami');
+    const { data: userLoggedInData, isLoading, error } = $api.useQuery('get', '/whoami');
     const { mobileGreeting, desktopGreeting } = useMemo(() => getGreetings(new Date().getHours()), []);
+    // const []
     return (
-        <header className="list-header">
-            <h3 className="list-header__greeting list-header__greeting--desktop">{desktopGreeting}</h3>
-            <h3 className="list-header__greeting list-header__greeting--mobile">{mobileGreeting}</h3>
-            {isLoading ? (
-                'loading'
+        <header className={css['list-header']}>
+            <h3 className={css['list-header__greeting']}>
+                <span className={css['list-header__greeting--desktop']}>
+                    {userLoggedInData?.user?.firstName && `Hi ${userLoggedInData.user.firstName}! `}
+                    {desktopGreeting}
+                </span>
+
+                <span className={css['list-header__greeting--mobile']}>
+                    {userLoggedInData?.user?.firstName && `Hi ${userLoggedInData.user.firstName}! `}
+                    {mobileGreeting}
+                </span>
+            </h3>
+            {error !== null ? (
+                <button disabled type="button" className={css.button__error}>
+                    Failed to fetch login!
+                </button>
+            ) : isLoading ? (
+                <button disabled type="button">
+                    Loading login status...
+                </button>
             ) : userLoggedInData !== undefined && userLoggedInData.user !== null ? (
-                <button onClick={logout}>sign out (sub:{userLoggedInData.user?.email})</button>
+                <button onClick={logout} type="button">
+                    <SignOut />
+                    Sign out ({userLoggedInData.user.email})
+                </button>
             ) : (
-                <button onClick={login}>sign in</button>
+                <button onClick={login} type="button">
+                    <GoogleIcon />
+                    Sign in with your CMU email
+                </button>
             )}
         </header>
     );
