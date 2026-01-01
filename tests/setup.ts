33433d5029f2c01d16c7cc9e56a89c667288c9c8
topshenyi-web <erxu@ctemc.org>
2025-12-31 19:30:46 -0500

    chore: remove rendundant matchers import

--- tests/setup.ts
+++ tests/setup.ts
@@ -3,7 +3,6 @@ import { expect, afterEach } from 'vitest';
 import { cleanup } from '@testing-library/react';
 import * as matchers from '@testing-library/jest-dom/matchers';
 
-import '@testing-library/jest-dom/vitest';
 expect.extend(matchers);
 
 // runs a cleanup after each test case (e.g. clearing jsdom)
