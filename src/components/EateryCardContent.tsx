94387416fc10d1357ec35eddb6e3faa8aef8aeae
Aromia <f2940015504@gmail.com>
2026-01-09 23:45:55 +0100

    feat: show pin icon on pinned card

--- src/components/EateryCardContent.tsx
+++ src/components/EateryCardContent.tsx
@@ -1,13 +1,16 @@
-import { MapPin } from 'lucide-react';
+import { Pin, MapPin } from 'lucide-react';
 import { ILocation_Full } from '../types/locationTypes';
 import css from './EateryCardContent.module.css';
 
 function EateryCardContent({ location, partOfMainGrid }: { location: ILocation_Full; partOfMainGrid: boolean }) {
-    const { location: physicalLocation, name } = location;
+    const { location: physicalLocation, name, cardViewPreference } = location;
 
     return (
         <div className={css['card-content-container']}>
-            <h3 className={css['location-name-text']}>{name}</h3>
+            <h3 className={css['location-name-text']}>
+                {cardViewPreference === 'pinned' && <Pin size={16} />}
+                {name}
+            </h3>
 
             <span className={css['physical-location-text']}>
                 <MapPin size={12} />
