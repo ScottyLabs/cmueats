ae08d672d3faac8068a0dc23793c7f2eb813db28
Aromia <f2940015504@gmail.com>
2026-01-17 19:16:50 -0500

    chore: remove date

--- src/components/banners/THBanner.tsx
+++ src/components/banners/THBanner.tsx
@@ -26,14 +26,14 @@ export default function Banner() {
                             <a href="https://register.tartanhacks.com/" target="_blank" rel="noreferrer">
                                 Register
                             </a>{' '}
-                            for TartanHack, CMU&apos;s largest Hackathon by ScottyLabs by Feb. 1st!
+                            for TartanHack, CMU&apos;s largest Hackathon by ScottyLabs!
                         </span>
                     </span>
                     <span className={css['welcome-banner__text--short']}>
                         <a href="https://register.tartanhacks.com/" target="_blank" rel="noreferrer">
                             Register
                         </a>{' '}
-                        for TartanHack by Feb. 1st!
+                        for TartanHack by ScottyLabs!
                     </span>
                 </div>
                 <div
