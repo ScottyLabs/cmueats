93761c6301d20b4a1526ba48ccb142864d0b35b1
cnk2024 <chloenahlin@gmail.com>
2026-04-18 16:34:36 -0400

    fix: mobile drawer width

--- src/contexts/IsMobileContext.tsx
+++ src/contexts/IsMobileContext.tsx
@@ -3,10 +3,10 @@ import { createContext, useContext, useState, useEffect } from 'react';
 const isMobileContext = createContext<boolean | undefined>(undefined);
 
 export function IsMobileContextProvider({ children }: { children: React.ReactNode }) {
-    const [isMobile, setIsMobile] = useState(window.innerWidth <= 980);
+    const [isMobile, setIsMobile] = useState(window.innerWidth <= 900);
     useEffect(() => {
         const handleResize = () => {
-            setIsMobile(window.innerWidth <= 980);
+            setIsMobile(window.innerWidth <= 900);
         };
 
         window.addEventListener('resize', handleResize);
