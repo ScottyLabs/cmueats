bb6d748c8a6b4b6cca967db46a5b4511df0623a9
laasyaaki <laasya.p.aki@gmail.com>
2026-04-19 14:50:36 -0400

    change prize wording

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -30,12 +30,11 @@ export default function Footer() {
                     <>
                         <p>
                             <span style={{ color: 'hsl(117.55deg 20% 83%)' }}>
-                                <b>CMUEats Raffle Details:</b> We are raffling off 3 $40 Amazon gift cards! To enter,
-                                leave a written review on any dining location above. You'll get one raffle entry per
-                                review left after 4/18 and before 5/1 (non-inclusive), for a maximum of 10 entries.
-                                Reviews must be legitimate dining reviews about your experience at the eatery. We
-                                reserve the right to disqualify any entry. If you win, we will reach out to you via
-                                email!
+                                <b>CMUEats Raffle Details:</b> We are raffling off 3 prizes worth $40! To enter, leave a
+                                written review on any dining location above. You'll get one raffle entry per review left
+                                after 4/18 and before 5/1 (non-inclusive), for a maximum of 10 entries. Reviews must be
+                                legitimate dining reviews about your experience at the eatery. We reserve the right to
+                                disqualify any entry. If you win, we will reach out to you via email!
                             </span>
                         </p>
                         <p>All times are displayed in Pittsburgh local time ({nowString}).</p>
