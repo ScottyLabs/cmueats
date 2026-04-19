dc8e070abc32e6e8497d009df042706ed0010242
laasyaaki <laasya.p.aki@gmail.com>
2026-04-19 10:27:43 -0400

    fix

--- src/components/ListPageHeader.tsx
+++ src/components/ListPageHeader.tsx
@@ -1,26 +1,25 @@
-import { useMemo } from "react";
-import { $api, login, logout } from "../api";
-import css from "./ListPageHeader.module.css";
-import { getGreetings } from "../util/greeting";
-import GoogleIcon from "../assets/google.svg?react";
-import SignOut from "../assets/control_buttons/signOut.svg?react";
-import { useThemeContext } from "../ThemeProvider";
-import mikuHeaderImg from "../assets/miku/miku-banner-2026.png";
+import { useMemo } from 'react';
+import { $api, login, logout } from '../api';
+import css from './ListPageHeader.module.css';
+import { getGreetings } from '../util/greeting';
+import GoogleIcon from '../assets/google.svg?react';
+import SignOut from '../assets/control_buttons/signOut.svg?react';
+import { useThemeContext } from '../ThemeProvider';
+import mikuHeaderImg from '../assets/miku/miku-banner-2026.png';
 
 function AprilFools() {
     return (
         <span>
-            <span style={{ color: "var(--logo-first-half)" }}>cmu</span>
-            <span style={{ color: "var(--logo-second-half)" }}>:eats </span>
+            <span style={{ color: 'var(--logo-first-half)' }}>cmu</span>
+            <span style={{ color: 'var(--logo-second-half)' }}>:eats </span>
             has been acquired by
-            <span style={{ color: "var(--yellow-300)" }}> El Gallo De Oro</span>
+            <span style={{ color: 'var(--yellow-300)' }}> El Gallo De Oro</span>
             <br />
-            <span style={{ fontSize: 24, color: "var(--black-500)" }}>
-                and thus will also be{" "}
-                {/* eslint-disable-next-line react/jsx-no-target-blank */}
+            <span style={{ fontSize: 24, color: 'var(--black-500)' }}>
+                and thus will also be {/* eslint-disable-next-line react/jsx-no-target-blank */}
                 <a
                     href="https://the-tartan.org/2026/02/16/el-gallo-to-close-after-29-years-of-operation/"
-                    className={css["april-fools-url"]}
+                    className={css['april-fools-url']}
                     target="_blank"
                     rel="noreferrer"
                 >
@@ -32,56 +31,40 @@ function AprilFools() {
 }
 
 export default function ListPageHeader() {
-    const {
-        data: userLoggedInData,
-        isLoading,
-        error,
-    } = $api.useQuery("get", "/whoami");
+    const { data: userLoggedInData, isLoading, error } = $api.useQuery('get', '/whoami');
     const { theme } = useThemeContext();
     const { mobileGreeting, desktopGreeting } = useMemo(
-        () =>
-            getGreetings(new Date().getHours(), {
-                isMikuDay: theme === "miku",
-            }),
-        [theme],
+        () => getGreetings(),
+        // new Date().getHours(), {
+        // isMikuDay: theme === 'miku',}
+        [
+            // theme
+        ],
     );
-    if (theme === "miku") {
-        return (
-            <img
-                className={css["list-header-miku"]}
-                src={mikuHeaderImg}
-                alt=""
-            />
-        );
+    if (theme === 'miku') {
+        return <img className={css['list-header-miku']} src={mikuHeaderImg} alt="" />;
     }
 
     return (
-        <header className={css["list-header"]}>
-            <h3 className={css["list-header__greeting"]}>
-                {theme === "april-fools" ? (
+        <header className={css['list-header']}>
+            <h3 className={css['list-header__greeting']}>
+                {theme === 'april-fools' ? (
                     <AprilFools />
                 ) : (
-                    <span
-                        role="button"
-                        className={css["list-header__greeting-button"]}
-                        onClick={() =>
-                            document
-                                .getElementById("footer")
-                                ?.scrollIntoView({ behavior: "smooth" })
-                        }
+                    <button
+                        className={css['list-header__greeting-button']}
+                        onClick={() => document.getElementById('footer')?.scrollIntoView({ behavior: 'smooth' })}
                     >
-                        <span className={css["list-header__greeting--desktop"]}>
-                            {userLoggedInData?.user?.firstName &&
-                                `Hi ${userLoggedInData?.user?.firstName}! `}
+                        <span className={css['list-header__greeting--desktop']}>
+                            {userLoggedInData?.user?.firstName && `Hi ${userLoggedInData?.user?.firstName}! `}
                             {desktopGreeting}
                         </span>
 
-                        <span className={css["list-header__greeting--mobile"]}>
-                            {userLoggedInData?.user?.firstName &&
-                                `Hi ${userLoggedInData?.user?.firstName}! `}
+                        <span className={css['list-header__greeting--mobile']}>
+                            {userLoggedInData?.user?.firstName && `Hi ${userLoggedInData?.user?.firstName}! `}
                             {mobileGreeting}
                         </span>
-                    </span>
+                    </button>
                 )}
             </h3>
             {error !== null ? (
@@ -92,8 +75,7 @@ export default function ListPageHeader() {
                 <button disabled type="button">
                     Loading login status...
                 </button>
-            ) : userLoggedInData !== undefined &&
-              userLoggedInData.user !== null ? (
+            ) : userLoggedInData !== undefined && userLoggedInData.user !== null ? (
                 <button onClick={logout} type="button">
                     <SignOut />
                     Sign out ({userLoggedInData.user.email})
