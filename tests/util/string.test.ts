52704a2dbb1b27cd375397d9bc8af9b0d5881b85
topshenyi-web <erxu@ctemc.org>
2026-09-07 22:03:38 -0400

    fix: C should be capitalized in location name

--- tests/util/string.test.ts
+++ tests/util/string.test.ts
@@ -1,15 +1,17 @@
-import { expect, test } from 'vitest';
-import toTitleCase from '../../src/util/string';
+import { expect, test } from "vitest";
+import toTitleCase from "../../src/util/string";
 
 test.each([
-    ['asdf', 'Asdf'],
-    ['so trUe', 'So True'],
-    ['o K k', 'o k k'],
-    ['火鍋', '火鍋'],
-    ['', ''],
-    ['   ', ''],
-    ['  asdf ', 'Asdf'],
-    ['ii', 'II'],
-])('toTitleCasetest', (before, after) => {
-    expect(toTitleCase(before)).toBe(after);
+  ["asdf", "Asdf"],
+  ["so trUe", "So True"],
+  ["o K k", "o k k"],
+  ["火鍋", "火鍋"],
+  ["", ""],
+  ["   ", ""],
+  ["  asdf ", "Asdf"],
+  ["ii", "II"],
+  ["Stephanie's - Market C", "Stephanie's - Market C"],
+  ["Market c At HEINZ Cafe", "Market C At Heinz Cafe"],
+])("toTitleCasetest", (before, after) => {
+  expect(toTitleCase(before)).toBe(after);
 });
